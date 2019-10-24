f1bcac2cf15fb5266ef3e0bdc491265467ecbaf5
Guilherme Belmonte <contact@belmonte.dev>
2019-10-24 00:17:01 -0300

    Create user migration and ecto schema

---
+++ lib/blog/users/user.ex
@@ -0,0 +1,21 @@
+defmodule Blog.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "users" do
+    field :username, :string
+    field :password_hash, :string
+
+    timestamps(:utc_datetime_usec)
+  end
+
+  @fields [:username, :password_hash]
+  @doc false
+  def changeset(%__MODULE__{} = user, params \\ %{}) do
+    user
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+  end
+
+end
--- priv/repo/migrations/20191023023333_create_users_table.exs
+++ priv/repo/migrations/20191023023333_create_users_table.exs
@@ -2,6 +2,14 @@ defmodule Blog.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
+    create table("users", primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :username, :string, null: false
+      add :password_hash, :string, null: false
 
+      timestamps(type: utc_datetime_usec)
+    end
+
+    create index(:users, [:username])
   end
 end
