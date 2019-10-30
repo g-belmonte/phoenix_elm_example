46f2b4a96bde3852d82921865958fe77c4142ca5
Guilherme Belmonte <contact@belmonte.dev>
2019-10-29 23:18:33 -0300

    Create Elm "Hello World" application

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -1,6 +1,78 @@
-module Main exposing (main)
+import Browser
+import Browser.Navigation as Navigation
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Url
 
-import Html exposing (text)
 
+-- MAIN
+
+
+main : Program () Model Msg
 main =
-    text "Hello Elm and Phoenix"
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Navigation.Key
+    , url : Url.Url
+    }
+
+
+init : () -> Url.Url -> Navigation.Key -> (Model, Cmd Msg)
+init flags url key =
+    ( Model key url, Cmd.none)
+
+
+-- UPDATE
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Navigation.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Navigation.load href )
+
+        UrlChanged url ->
+             ( { model | url = url }
+             , Cmd.none
+             )
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Phoenix and Elm"
+    , body =
+        [ text "Hello world" ]
+    }
