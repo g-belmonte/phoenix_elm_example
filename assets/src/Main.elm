0cb84b783b6daa52194982ca4c6ecfd2baf92a57
Guilherme Belmonte <contact@belmonte.dev>
2019-11-17 19:27:59 -0300

    Add Navbar and fake list of posts on Elm

--- assets/src/Main.elm
+++ assets/src/Main.elm
@@ -1,6 +1,7 @@
 import Browser
 import Browser.Navigation as Navigation
-import Html exposing (text)
+import Html as H exposing (Html, text)
+import Html.Attributes as A
 import Url
 
 -- =============================================================================
@@ -82,6 +83,38 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Phoenix and Elm"
     , body =
-        [ text "Hello world" ]
+        [ viewNavbar ()
+        , viewFeed ()
+        ]
     }
 
+viewNavbar : () -> Html msg
+viewNavbar _ =
+    H.nav []
+        [ H.div [ A.class "nav-wrapper purple darken-1"]
+              [ H.ul [ A.class "left" ]
+                    [ H.li [] [ H.a [ A.href "#" ] [ H.i [ A.class "material-icons" ] [ text "menu" ] ] ] ]
+              , H.a [ A.href "#", A.class "brand-logo center" ] [ text "Logo" ]
+              , H.ul [ A.class "right" ]
+                  [ H.li [] [ H.a [ A.href "#" ] [ H.i [ A.class "material-icons" ] [ text "person" ] ] ] ]
+              ]
+        ]
+
+viewFeed : () -> Html msg
+viewFeed _ =
+    H.div []
+        [ H.table []
+              [ H.thead []
+                    [ H.tr []
+                          [ H.th [] [ text "Post" ]
+                          , H.th [] [ text "Author" ]
+                          ]
+                    ]
+              , H.tbody []
+                  [ H.tr []
+                        [ H.td [] [ text "this is my post" ]
+                        , H.td [] [ text "Belmonte" ]
+                        ]
+                  ]
+              ]
+        ]
